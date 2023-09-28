cbbc6eae7f8907e30761c7d80ec1dcfca7262ed5
Rhone06 <146122568+Rhone06@users.noreply.github.com>
2023-09-28 14:49:30 -0400

    Create collections.py

---
+++ computer_science_exercises/02_collections/collections.py
@@ -0,0 +1,11 @@
+# Collections Example, Ja'Shaun Rhone, v0.0 
+
+# LIST -- ORDERED, CHANGABLE, ALLOWS DUPLICATE VALUES
+bluecharacters = ["Sonic", "Sans", "Stich", "Megamind", "Bubbles"]
+# Each item on the list is known as an ELEMENT
+# The position in the list for each is the INDEX
+# The element "SONIC" is at index 0.
+# Python Only: index -1 it is the last item on the list.
+testScores = [100, 96, 42, 65, 45, 78]
+classGpa = [2.13, 5.21, .67, 8.90, .21, 3.34]
+
